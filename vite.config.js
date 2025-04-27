73aa72eaa03b84427abf7c05b3b8f11170daaf93
Jorge-pereava <56284996+Jorge-pereava@users.noreply.github.com>
2025-04-27 00:41:43 -0500

    Se modifican para validar que haga bien el deployment

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
 
-export default {
-  base: '/'
-};
+// export default {
+//   base: '/'
+// };
