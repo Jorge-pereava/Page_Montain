7d714ee0f1ed7813a2f5390f5f4cd7601f172c53
Jorge-pereava <56284996+Jorge-pereava@users.noreply.github.com>
2025-05-11 00:16:19 -0500

    Nueva imagen en la seccion de imagenes

--- src/components/ImageSeccion.jsx
+++ src/components/ImageSeccion.jsx
@@ -1,11 +1,11 @@
-import Almohada from "../assets/Almohada6.jpg"
+import Colchon from "../assets/Colchon.png"
 
 export default function ImageSeccion() {
     return (
       <section className="bg-white py-10 px-6 text-center">
         <div className="max-w-4xl mx-auto">
           <img
-            src= {Almohada} // Imagen local, asegúrate que esté en /public/images/
+            src= {Colchon}
             alt="Colchón moderno flotante"
             className="mx-auto w-full max-w-xs md:max-w-sm lg:max-w-md rounded-2xl shadow-lg transition-transform duration-500 hover:scale-105"
           />
